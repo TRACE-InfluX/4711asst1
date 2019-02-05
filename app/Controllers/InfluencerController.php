150a6a6c68b7999637aade039259b71e6cd2ac00
Carlo Mendoza <carlomendoza89@gmail.com>
2019-02-05 11:48:58 -0800

    added base url

--- app/Controllers/InfluencerController.php
+++ app/Controllers/InfluencerController.php
@@ -18,7 +18,6 @@ class InfluencerController extends Controller {
         $model = new Influencer();
         $students = $model->findAll();
         return $this->response->setStatusCode(200)->setJSON($students);
-        //return $this->respond($students, 200, 'yeet');
     }
 
     public function show($id) {
